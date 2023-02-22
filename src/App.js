7229a3f5eabbaa04937bbd91a6f4c480218e1b2d
Piyush Yadav <piyushyadav1105@gmail.com>
2023-02-22 12:24:35 +0530

    style and function fixed

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
 import React, { useState } from "react";
+import "./App.css";
 import { EmojiPicker, Emoji } from "react-emoji-search";
 const App = () => {
   const [emoji, setEmoji] = useState();
+  const [colorMode, setColorMode] = useState("light");
+  const modeToggle = () => {
+    colorMode === "light" ? setColorMode("dark") : setColorMode("light");
+  };
+
   return (
-    <div>
-      {/* <EmojiPicker set="google" /> */}
+    <>
+      <div className={`main-container background-${colorMode}`}>
+        <button
+          className={`btn btn-1 btn-1e btn-1e-${colorMode}`}
+          onClick={modeToggle}
+        >
+          Color Mode {colorMode}
+        </button>
+        <Emoji unicode={emoji} set="google" size={50} />
+        <div style={{ height: 600, width: 700 }}>
+          {/* <EmojiPicker set="google" /> */}
 
-      {/* <EmojiPicker set="google" emojiSize={24} emojiSpacing={8} /> */}
-      {/* <EmojiPicker emojiVersion={12.0} /> */}
-      {/* <EmojiPicker onEmojiClick={(emoji) => setEmoji(emoji)} /> */}
-      <EmojiPicker
-        onEmojiClick={(emoji) => setEmoji(emoji)}
-        set="google"
-        styles={{
-          backgroundColor: "#2e4960",
-          indicatorColor: "#b04c2d",
-          fontColor: "lightgrey",
-          searchBackgroundColor: "#263d51",
-          tabsFontColor: "#8cdce4",
-          searchFontColor: "lightgrey",
-          skinTonePickerBackgroundColor: "#284155",
-        }}
-      />
+          {/* <EmojiPicker set="google" emojiSize={24} emojiSpacing={8} /> */}
+          {/* <EmojiPicker emojiVersion={12.0} /> */}
+          {/* <EmojiPicker onEmojiClick={(emoji) => setEmoji(emoji)} /> */}
 
-      <Emoji />
-      <Emoji unicode={emoji} set="google" size={50} />
-    </div>
+          <EmojiPicker
+            onEmojiClick={(emoji) => setEmoji(emoji)}
+            set="google"
+            // styles={{
+            //   backgroundColor: "#2e4960",
+            //   indicatorColor: "#b04c2d",
+            //   fontColor: "lightgrey",
+            //   searchBackgroundColor: "#263d51",
+            //   tabsFontColor: "#8cdce4",
+            //   searchFontColor: "lightgrey",
+            //   skinTonePickerBackgroundColor: "#284155",
+            // }}
+            mode={colorMode}
+          />
+        </div>
+      </div>
+    </>
   );
 };
 
